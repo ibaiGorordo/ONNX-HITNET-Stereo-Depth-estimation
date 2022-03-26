9ec855f78481d59318cac65da601465acefb529d
Ibai <ibai.gorordo@hotmail.com>
2022-03-26 18:57:59 +0900

    Update driving_stereo_test.py

--- driving_stereo_test.py
+++ driving_stereo_test.py
@@ -38,8 +38,6 @@ driving_stereo_path = "drivingStereo"
 start_sample = 700
 left_images, right_images, depth_images = get_driving_stereo_images(driving_stereo_path, start_sample)
 
-out = cv2.VideoWriter('outpy2.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 20, (879*2,400))
-
 cv2.namedWindow("Estimated depth", cv2.WINDOW_NORMAL)	
 for left_path, right_path, depth_path in zip(left_images, right_images, depth_images):
 
@@ -47,7 +45,7 @@ for left_path, right_path, depth_path in zip(left_images, right_images, depth_im
 	left_img = cv2.imread(left_path)
 	right_img = cv2.imread(right_path)
 	depth_img = cv2.imread(depth_path, cv2.IMREAD_UNCHANGED).astype(np.float32)/1000
-	
+
 	# Estimate the depth
 	disparity_map = depth_estimator(left_img, right_img)
 
@@ -55,12 +53,10 @@ for left_path, right_path, depth_path in zip(left_images, right_images, depth_im
 	color_real_depth = depth_estimator.util_draw_depth(depth_img, (left_img.shape[1], left_img.shape[0]), max_distance)
 	cobined_image = np.hstack((left_img, color_real_depth, color_depth))
 
-	out.write(cobined_image)
 	cv2.imshow("Estimated depth", cobined_image)
 
 	# Press key q to stop
 	if cv2.waitKey(1) == ord('q'):
 		break
 
-out.release()
 cv2.destroyAllWindows()
